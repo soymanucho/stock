bc20aa47c2854a484e79fbdba5d84cfea132da9f
Manu <alvarez.jmanuel94@gmail.com>
2020-12-15 19:01:55 -0300

    prueba1

--- deploy.sh
+++ deploy.sh
@@ -6,14 +6,6 @@ php artisan down
 # update source code
 git pull
 
-# update PHP dependencies
-export COMPOSER_HOME='/tmp/composer'
-composer install --no-interaction --no-dev --prefer-dist
-	# --no-interaction	Do not ask any interactive question
-	# --no-dev		Disables installation of require-dev packages.
-	# --prefer-dist		Forces installation from package dist even for dev versions.
-
-
 # clear cache
 php artisan cache:clear
 
