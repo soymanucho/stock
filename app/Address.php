dd339681996e9a48da72cf87e95a55428be90928
Manu <alvarrez.jmanuel94@gmail.com>
2021-01-27 19:01:08 -0300

    comit

--- app/Address.php
+++ app/Address.php
@@ -22,6 +22,11 @@ class Address extends Model
     return $this->belongsTo(Location::class)->withTrashed();
   }
 
+  public function FunctionName($value='')
+  {
+    return 'sarasa';
+  }
+
   public function client()
   {
     return $this->belongsTo(Client::class);
