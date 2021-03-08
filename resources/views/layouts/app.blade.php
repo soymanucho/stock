2f794dc85308909f92481b76d36c1a3285700c3d
soymanucho <alvarez.jmanuel94@gmail.com>
2021-03-08 14:54:07 -0300

    f

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,7 +11,8 @@
 	<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 	<script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
 	<link href="{{ asset('/css/icomoonstyles.css') }}" rel="stylesheet" type="text/css">
-	<link href="{{ asset('/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css">
+	{{-- <link href="{{ asset('/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css"> --}}
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet">
 	<link href="{{ asset('/css/bootstrap_limitless.min.css') }}" rel="stylesheet" type="text/css">
 	<link href="{{ asset('/css/layout.min.css') }}" rel="stylesheet" type="text/css">
 	<link href="{{ asset('/css/components.min.css') }}" rel="stylesheet" type="text/css">
@@ -28,7 +29,8 @@
 	<link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
 	{{-- <script src="{{ asset('/js/jquery.min.js') }}" defer></script> --}}
 	<!-- Core JS files -->
-	<script src="{{ asset('/js/bootstrap.bundle.min.js') }}" defer></script>
+	{{-- <script src="{{ asset('/js/bootstrap.bundle.min.js') }}" defer></script> --}}
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js"></script>
 	<script src="{{ asset('/js/blockui.min.js') }}" defer></script>
 
 	<script src="{{ asset('/js/pdfmake.min.js') }}" defer></script>
