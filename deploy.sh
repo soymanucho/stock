f88d8ec8cd8c66a47eefbdcc8ea14622859b58b6
soymanucho <alvarez.jmanuel94@gmail.com>
2021-03-08 23:21:57 -0300

    f

--- deploy.sh
+++ deploy.sh
@@ -13,14 +13,14 @@ composer install --no-interaction --no-dev --prefer-dist
 	# --no-dev		Disables installation of require-dev packages.
 	# --prefer-dist		Forces installation from package dist even for dev versions.
 
-# clear cache
-php artisan cache:clear
-
-# clear config cache
-php artisan config:clear
-
-# cache config
-php artisan config:cache
+# # clear cache
+# php artisan cache:clear
+#
+# # clear config cache
+# php artisan config:clear
+#
+# # cache config
+# php artisan config:cache
 
 # restart queues
 php artisan -v queue:restart
