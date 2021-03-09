486d932502cede55a89a486fff57bdd5b7080b80
soymanucho <alvarez.jmanuel94@gmail.com>
2021-03-09 00:00:52 -0300

    s

--- deploy.sh
+++ deploy.sh
@@ -1,40 +1,40 @@
 #!/bin/sh
 
 # activate maintenance mode
-php artisan down
+sudo php artisan down
 
 # update source code
-git pull
+sudo git pull
 
 # update PHP dependencies
-composer install --no-interaction --no-dev --prefer-dist
+sudo composer install --no-interaction --no-dev --prefer-dist
 	# --no-interaction	Do not ask any interactive question
 	# --no-dev		Disables installation of require-dev packages.
 	# --prefer-dist		Forces installation from package dist even for dev versions.
-composer dump-autoload
+sudo composer dump-autoload
 
-composer cc
+sudo composer cc
 # update database
-php artisan migrate --force
+sudo php artisan migrate --force
 # --force		Required to run when in production.
 
 # clear cache
-php artisan cache:clear
+sudo php artisan cache:clear
 
 # clear config cache
-php artisan config:clear
-php artisan config:cache
+sudo php artisan config:clear
+sudo php artisan config:cache
 
-php artisan view:clear
-php artisan view:cache
+sudo php artisan view:clear
+sudo php artisan view:cache
 
-php artisan route:clear
-php artisan route:cache
+sudo php artisan route:clear
+sudo php artisan route:cache
 
-php artisan event:clear
-php artisan event:cache
+sudo php artisan event:clear
+sudo php artisan event:cache
 # restart queues
-php artisan -v queue:restart
+sudo php artisan -v queue:restart
 
 # stop maintenance mode
-php artisan up
+sudo php artisan up
