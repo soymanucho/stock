e95704b29bfaa75f47be0ad9ae545f537d97b2a4
soymanucho <alvarez.jmanuel94@gmail.com>
2021-03-09 00:41:05 -0300

    f

--- deploy.sh
+++ deploy.sh
@@ -15,16 +15,10 @@ sudo php artisan cache:clear
 
 # clear config cache
 sudo php artisan config:clear
-sudo php artisan config:cache
 
 sudo php artisan view:clear
-sudo php artisan view:cache
 
 sudo php artisan route:clear
-sudo php artisan route:cache
-
-sudo php artisan event:clear
-sudo php artisan event:cache
 # restart queues
 sudo php artisan -v queue:restart
 
