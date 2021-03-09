31a7cb73f4f704f50d0fd2aa1bf9f82e72be8741
soymanucho <alvarez.jmanuel94@gmail.com>
2021-03-08 23:37:17 -0300

    fix

--- deploy.sh
+++ deploy.sh
@@ -7,27 +7,26 @@ php artisan down
 git pull
 
 # update PHP dependencies
-export COMPOSER_HOME='/tmp/composer'
 composer install --no-interaction --no-dev --prefer-dist
 	# --no-interaction	Do not ask any interactive question
 	# --no-dev		Disables installation of require-dev packages.
 	# --prefer-dist		Forces installation from package dist even for dev versions.
 
-# # clear cache
-# php artisan cache:clear
-#
-# # clear config cache
-# php artisan config:clear
-#
-# # cache config
-# php artisan config:cache
+# update database
+php artisan migrate --force
+# --force		Required to run when in production.
+
+# clear cache
+php artisan cache:clear
+
+# clear config cache
+php artisan config:clear
+
+# cache config
+php artisan config:cache
 
 # restart queues
 php artisan -v queue:restart
-
-# update database
-php artisan migrate --force
-	# --force		Required to run when in production.
 
 # stop maintenance mode
 php artisan up
