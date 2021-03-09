bac109332c088968b06261905f0fc026212dae76
soymanucho <alvarez.jmanuel94@gmail.com>
2021-03-08 23:41:37 -0300

    fix

--- deploy.sh
+++ deploy.sh
@@ -11,7 +11,9 @@ composer install --no-interaction --no-dev --prefer-dist
 	# --no-interaction	Do not ask any interactive question
 	# --no-dev		Disables installation of require-dev packages.
 	# --prefer-dist		Forces installation from package dist even for dev versions.
+composer dump-autoload
 
+composer cc
 # update database
 php artisan migrate --force
 # --force		Required to run when in production.
@@ -21,10 +23,16 @@ php artisan cache:clear
 
 # clear config cache
 php artisan config:clear
-
-# cache config
 php artisan config:cache
 
+php artisan view:clear
+php artisan view:cache
+
+php artisan route:clear
+php artisan route:cache
+
+php artisan event:clear
+php artisan event:cache
 # restart queues
 php artisan -v queue:restart
 
